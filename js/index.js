eb40ee8f6b3134ad22449490ef0f2a83a9f815f8
xushirong <2277503428@qq.com>
2016-11-10 22:46:42 +0800

    aa

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//->解决click的300ms延迟
+FastClick.attach(document.body);
+
+//->动态计算REM的值
+/*~function () {
+    var winW = document.documentElement.clientWidth;
+    document.documentElement.style.fontSize = winW / 750 * 100 + "px";
+}();*/
+
+
+
+
+
+
+
+
+
